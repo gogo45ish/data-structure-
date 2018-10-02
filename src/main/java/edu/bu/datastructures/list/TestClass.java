1c8d174e33b8c1ef687cffe5ea26fb668ac07064
gogo45ish <201706330@bethlehem.edu>
2018-10-02 14:30:56 +0300

    second test

--- src/main/java/edu/bu/datastructures/list/TestClass.java
+++ src/main/java/edu/bu/datastructures/list/TestClass.java
@@ -1,5 +1,11 @@
 package edu.bu.datastructures.list;
 
 public class TestClass {
-
+public static void main(String[]args) {
+	System.out.println("HELLO");
+	System.out.println("HELLO");
+	System.out.println("HELLO");
+	System.out.println("HELLO");
+	
+}
 }
